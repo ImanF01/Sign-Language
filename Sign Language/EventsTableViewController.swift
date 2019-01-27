bec6b314a8befe47bd4ffc810761c1f478af6587
ImanF01 <imanfahmy01@gmail.com>
2019-01-26 16:03:53 -0800

    connected eventstableviewcell

--- Sign Language/EventsTableViewController.swift
+++ Sign Language/EventsTableViewController.swift
@@ -21,7 +21,7 @@ class EventsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "eventsTableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "eventsTableViewCell", for: indexPath) as! EventsTableViewCell
     //    cell.textLabel?.text = "Cell Row: \(indexPath.row) Section: \(indexPath.section)"
         
         return cell
